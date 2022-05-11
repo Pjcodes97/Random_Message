aae7c98d8d1dc2cca4ab9523a530548ecf10fd80
PJcodes <104080524+Pjcodes97@users.noreply.github.com>
2022-05-11 10:19:28 -0400

    Edit grammar and add message creation function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Array of starting portion of the message, will deteremine what category message will be created.
-const baseLine = ['Never', 'Only you can', 'Remember:', 'You will be', 'You are'];
+let baseLine = ['NEVER', 'Only you can', 'Remember,', 'You will be', 'You are'];
 
 // Object containing lists of endlines with do diff categories, if baseline[x] < 3 then use category 1, if baseline[x] >=3 use category 2, with one extra exception.
 const endLine = {
@@ -29,9 +29,29 @@ console.log(negOrPos);
 
 //Decides which baseLine to use
 let baseSelect = Math.floor(Math.random() * 5);
+console.log(baseSelect);
 
+//Creates random message using variables created so far.
 const messageCreator = (decider, bases) => {
-    
-}
+    let randMessage = '';
+    if (bases < 3) {
+        let endSelect = endLine.categoryOne[Math.floor(Math.random() * 7)];
+        console.log(endSelect)
+        randMessage = baseLine[bases] + " " + endSelect;
+    } else if (bases >= 3 && decider === 0) {                                 //This section modifies the category 2 outcomes to add a negative option to them, adding more randomization.
+        let endSelect = endLine.catergoryTwo[Math.floor(Math.random() * 7)];
+        console.log(endSelect);
+        baseLine[3] = 'You will NEVER be';
+        baseLine[4] = 'You are NEVER';
+        randMessage = baseLine[bases] + " " + endSelect;
+    } else if (bases >= 3 && decider === 1) {
+        let endSelect = endLine.catergoryTwo[Math.floor(Math.random() * 7)];
+        console.log(endSelect);
+        randMessage = baseLine[bases] + " " + endSelect;
+    };
+    return randMessage;
+};
+
+console.log(messageCreator(negOrPos, baseSelect));
 
 
